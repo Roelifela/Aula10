474a29c8f1c9fd3f3a0cad0e4b42a27ffda0b67d
Roelifela <47600839+Roelifela@users.noreply.github.com>
2019-04-17 22:22:18 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,50 @@
+package br.com.digitalhouse;
+
+public class Pessoa {
+
+    private String nome;
+    private int rg;
+
+    public Pessoa(String nome, int rg) {
+        this.nome = nome;
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    @Override
+    public boolean equals(Object mesmaPessoa) {
+
+        if (!(mesmaPessoa instanceof Pessoa)){
+            return false;
+        }
+
+
+        //mesmaPessoa.getRg  PARA PEGAR O CODIGO ABAIXO.
+
+        if (((Pessoa) mesmaPessoa).getRg() == this.getRg() && ((Pessoa) mesmaPessoa).getNome().equals(this.getNome())) {
+
+            return true;
+
+        } else {
+            return false;
+        }
+
+
+
+    }
+}
